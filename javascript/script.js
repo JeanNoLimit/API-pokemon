6f66ab425522b1f4fd4ab942431e9d705951c70e
Jean-Noel <jean.noel.richert@gmail.com>
2023-06-09 10:25:59 +0200

    On réinitialise le résultat à chaque génération

--- javascript/script.js
+++ javascript/script.js
@@ -1,8 +1,10 @@
 const form = document.querySelector("form");
+const nbPokemon = document.querySelector(".nbPokemonsJs");
+
+
+
 form.addEventListener("submit", (event) => {
     event.preventDefault();
-    const nbPokemon = document.querySelector(".nbPokemonsJs");
-   
     genererPokemon(nbPokemon);
 })
 
@@ -13,23 +15,28 @@ function genererPokemon(nbPokemon) {
     fetch("https://pokeapi.co/api/v2/pokemon?limit="+nbPokemon.value+"&offset=0")
     .then((response) => response.json())
     .then((data)=>{
-    //    console.log(data);
+       console.log(data);
+
+         // On rattache l'élément au document
+        const result = document.querySelector(".conteneurAPokemons");
+        result.innerText= null;
 
         for(let i=0;i<nbPokemon.value;i++){
 
-            // On rattache l'élément au document
-            const result = document.querySelector(".conteneurAPokemons");
+          
+
             // création d'une balise dédiée à recevoir les infos 
             const pokemonElement = document.createElement("article");
 
             // Création d'une balise qui contiendra le nom du pokemon
             const nomPokemon = document.createElement("p");
             nomPokemon.className = "nomPokemon";
-
             // Contenu de la balise. On place le nom du pokemon dansu ne variable tmp pour afficher la première lettre en majuscule.
             var nomTmp =  data.results[i].name
-            console.log(nomTmp)
             nomPokemon.innerText= nomTmp[0].toUpperCase() + nomTmp.slice(1);
+
+
+
 
             //Idem pour l'image
             const imgPokemon = document.createElement("img");
